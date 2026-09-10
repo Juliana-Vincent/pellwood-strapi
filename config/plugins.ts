04dc8063e29c5c584e036eb9fb1b6e61701e8c42
Juliana <vincent@Juliana-MacBook-Pro.local>
2026-09-10 19:23:54 +0200

    adding cloudinary

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,21 @@
 import type { Core } from '@strapi/strapi';
 
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({});
+const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('yqzar4su'),
+        api_key: env('917116237663665'),
+        api_secret: env('up1vdiH2hnjznqFvE1OcvHxYjA0'),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
 
 export default config;
